e01264bedf4f35f3831d0afd0df9c75ffb9a86b8
David Searle <dsearle2@gmail.com>
2022-04-26 20:23:29 +0200

    ShieldChange

--- Assets/Scripts/Ship/Shield/ShieldUI.cs
+++ Assets/Scripts/Ship/Shield/ShieldUI.cs
@@ -14,12 +14,12 @@ public class ShieldUI : MonoBehaviour {
     }
 
     public void SetIndicator(float health, float distribution) {
-        limit.Radius = distribution * 3.5f;
-        indicator.Radius = (health / distribution) * (limit.Radius - 0.15f) - health / 2f;
-        indicator.Thickness = health;
+        //limit.Radius = distribution * 3.5f;
+        //indicator.Radius = (health / distribution) * (limit.Radius - 0.15f) - health / 2f;
+        //indicator.Thickness = health;
 
-        //indicator.Thickness = Mathf.Min(distribution, health);
-        //indicator.Radius = health / distribution * (3 + 0.25f - distribution / 2f);
+        indicator.Thickness = Mathf.Min(distribution, health);
+        indicator.Radius = health / distribution * (3 + 0.25f - distribution / 2f);
     }
 
 }
